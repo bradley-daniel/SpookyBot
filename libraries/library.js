e10b9618dea9dd72088e1de873933246372e51a1
Bradley Daniel <63433442+Bradley-Daniel@users.noreply.github.com>
2022-04-04 09:48:47 -0400

    node files update

--- libraries/library.js
+++ libraries/library.js
@@ -1,6 +1,4 @@
 fs = require('fs');
-
-
 function getValidPermissions(command, message) {
 	const validPermissions = ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_GUILD', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'PRIORITY_SPEAKER', 'STREAM', 'VIEW_CHANNEL', 'SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'USE_EXTERNAL_EMOJIS', 'VIEW_GUILD_INSIGHTS', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES', 'MANAGE_ROLES', 'MANAGE_WEBHOOKS', 'MANAGE_EMOJIS'];
 	const invalidPerms = [];
@@ -54,8 +52,6 @@ async function sendTemporaryMessage(message, contents, time) {
 	const tempMessage = await message.channel.send(contents);
 	tempMessage.delete({ timeout: time });
 }
-
-
 module.exports.formatString = formatString;
 module.exports.deleteUpTo = deleteUpTo;
 module.exports.setReactionSystem = setReactionSystem;
