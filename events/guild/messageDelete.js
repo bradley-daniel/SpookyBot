42db1fbb792e49b96f63c408d9ea8e4890907d07
Bradley Daniel <63433442+Bradley-Daniel@users.noreply.github.com>
2022-12-21 20:03:42 -0500

    modified .gitignore

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,6 +1,8 @@
 module.exports = async (Discord, client, message) => {
     let messageDeleted = new Date().getTime();
     if(message.mentions.users.size > 0 && messageDeleted - message.createdTimestamp < 3600000){
-        message.channel.send('<@' + message.author.id + '> please do not act like a feral animal by ghost pinging!');
+        message.channel.send('<@' + message.author.id + '> please do not act like a feral animal!');
+        let member = message.guild.members.cache.get(message.author.id);
+        await member.timeout(3600000, 'Ghost pinging');
     }
 }
